18e1b774173f5959106529e13106d20673cee58b
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-10-29 17:25:41 +0200

    Fixed atype annotation in BaseRobot constructor method

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class BaseRobot:
         self,
         name: str,
         weight: int,
-        coords: list[int] = None
+        coords: list[int] | None = None
     ) -> None:
         self.name = name
         self.weight = weight
